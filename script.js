0db4a980cfb5364a410cc1215bc749390fa325d2
Anthonycaps2 <71470958+Anthonycaps2@users.noreply.github.com>
2023-04-26 18:59:55 -0400

    INST377 Final Project Name Filter

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const pokedex = document.getElementById("pokedex");
 
-console.log(pokedex);
+const radar = document.getElementById('radar-chart');
+
+pokemon_global = null;
 
 const fetchPokemon = () => {
 
@@ -16,18 +18,87 @@ const fetchPokemon = () => {
             name: data.name,
             id: data.id,
             image: data.sprites['front_default'],
-            type: data.types.map((type) => type.type.name).join(', ')
+            type: data.types.map((type) => type.type.name).join(', '),
+            stats: [data.stats[0].base_stat, data.stats[1].base_stat, data.stats[2].base_stat, data.stats[3].base_stat, data.stats[4].base_stat, data.stats[5].base_stat] 
         }));
-        displayPokemon(pokemon);
+        
+        pokemon_global = pokemon;
     });
 };
+
+function RADAR(pokemon) {
+    new Chart(radar, {
+        type: 'radar',
+        data: {
+          labels: ['HP', 'Atk', 'Def', 'SpA', 'SpD', 'Speed'],
+          datasets: [{
+            data: pokemon.stats,
+            backgroundColor: ['lightgrey'],
+            pointBackgroundColor: ['yellow', 'aqua', 'pink', 'lightgreen', 'lightblue', 'gold'],
+            borderColor: ['black'],
+            borderWidth: 1,
+            pointRadius: 6,
+          }]
+        },
+        options: {
+            responsive: false,
+            elements: {
+               line: {
+                  borderWidth: 3
+               }
+            }
+        }
+    });
+}
+
+async function mainEvent() {
+    const mainForm = document.querySelector('.main_form');
+    const loadDataButton = document.querySelector('#data_load');
+    const search = document.querySelector('#data_filter');
+    const textField = document.querySelector('#filter');
+    console.log(textField);
+
+    fetchPokemon();
+    
+    loadDataButton.addEventListener('click', async (submitEvent) => { // async has to be declared on every function that needs to "await" something    
+        console.log('loading Pokemon'); 
+        displayPokemon(pokemon_global);
+    });
+
+        
+    
+    textField.addEventListener('input', (event) => {
+        text_store = event.target.value
+        console.log(text_store);
+        search.addEventListener('click', (event) => {
+            console.log('click', ' ', text_store);
+            displayOnePokemon(text_store);
+        })
+    })
+}
+
+const displayOnePokemon = (name) => {
+    console.log(name);
+    position = 0;
+    for (let i = 0; i < pokemon_global.length; i++) {
+        console.log(name, ' ', pokemon_global[i].name)
+        if(name == pokemon_global[i].name){
+            position = i;
+            break;
+        } else{
+            console.log('false');
+        }
+      }
+     
+      
+}
 
 const displayPokemon = (pokemon) => {
     console.log(pokemon);
     const pokemonHTMLString = pokemon.map ( pokeman =>
         `
     <li class="card">
-        <img class="card-image" src="${pokeman.image}" /> 
+        <img class="card-image" src="${pokeman.image}" />
         <h2>${pokeman.id}. ${pokeman.name}</h2>
         <p> Type: ${pokeman.type}</p>
     </li>
@@ -36,4 +107,8 @@ const displayPokemon = (pokemon) => {
     pokedex.innerHTML = pokemonHTMLString;
 }
 
-fetchPokemon();
+
+
+
+
+document.addEventListener('DOMContentLoaded', async () => mainEvent());
